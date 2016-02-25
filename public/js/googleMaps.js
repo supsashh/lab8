84fdc78b2fe7cb1ce0854dc70cf731deb5945957
Alexander Goldshteyn <asgold8@yahoo.com>
2016-02-25 18:04:31 +0000

    Third party APIs

---
+++ public/js/googleMaps.js
@@ -0,0 +1,17 @@
+function initMap() {
+  // Create center marker at UCSD
+  var ucsd_ltlng = {lat:32.884991, lng: -117.241318};
+  
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: ucsd_ltlng,
+    zoom: 18
+  });
+
+  var marker = new google.maps.Marker({
+      position: ucsd_ltlng,
+      map: map,
+      title: 'UCSD'
+  });
+}
